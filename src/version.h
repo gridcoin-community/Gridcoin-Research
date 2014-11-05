75f44499f92262e7132f1c42227bbfe406b3e40a
gridcoin <contact@gridcoin.us>
2014-11-05 10:06:33 -0600

    Incremented Protocol Version

--- src/version.h
+++ src/version.h
@@ -24,9 +24,9 @@ extern const std::string CLIENT_DATE;
 ///////////////////////////////////////////////////////////
 // network protocol versioning                           //
 //                                                       //
-static const int PROTOCOL_VERSION =       180109;        //
+static const int PROTOCOL_VERSION =       180110;        //
 // disconnect from peers older than this proto version   //
-static const int MIN_PEER_PROTO_VERSION = 180109;        // 
+static const int MIN_PEER_PROTO_VERSION = 180110;        // 
 ///////////////////////////////////////////////////////////
 
 //
