90fb729c754aa93f3a9a71d95bd19af9ca75f511
gridcoin <contact@gridcoin.us>
2014-11-27 07:18:19 -0600

    Orphan Problems3

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -424,8 +424,8 @@ static bool CheckStakeKernelHashV1(unsigned int nBits, const CBlock& blockFrom, 
 	//CBigNum bnCoinDayWeight = CBigNum(nValueIn + (5*COIN) ) * GetWeight((int64_t)txPrev.nTime, (int64_t)nTimeTx) / COIN / (24 * 60 * 60);
 	int64_t NewbieStakeWeightModifier = 0;
 	//double mint = (blockFrom.vtx[1].GetValueOut())/COIN;
-	double subsidy = (blockFrom.vtx[0].GetValueOut())/COIN;
-	std::string sSubsidy = RoundToString(subsidy,4);
+	//	double subsidy = (blockFrom.vtx[0].GetValueOut())/COIN;
+	//	std::string sSubsidy = RoundToString(subsidy,4);
 
 	int NC = NewbieCompliesWithFirstTimeStakeWeightRule(blockFrom,hashBoinc);
 	msMiningErrors2 = RoundToString(NC,0);
@@ -535,7 +535,7 @@ static bool CheckStakeKernelHashV1(unsigned int nBits, const CBlock& blockFrom, 
 			if (!checking_local)
 			{
 				//Dont print all this crap in the log if we are running a local miner:
-				printf("Block does not meet target: hashboinc %s Subsidy: %s\r\n",hashBoinc.c_str(), sSubsidy.c_str());
+				printf("Block does not meet target: hashboinc %s\r\n",hashBoinc.c_str());
 				printf("Researcher vitals: NC %u, cpid %s, project %s, accuracy %f \r\n",NC,cpid.c_str(),boincblock.projectname.c_str(),mag_accuracy);
 			}
 			return false;
