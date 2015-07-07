477e8512b85c394678e7479f2061fa2bd818d976
Gridcoin <contact@gridcoin.us>
2015-07-07 12:37:30 -0500

    3.4.2.6
    Gridcoin Research 3.4.2.6/MSI=25.3/Minor_Version=278
    Mandatory Upgrade
    
    - Coin will not load a superblock with avg mag < 10
    - Coin skips by corrupted beacons
    - added execute superblockaverage
    (Last valid superblock is re-loaded now)

--- src/version.h
+++ src/version.h
@@ -25,9 +25,9 @@ extern int MINOR_VERSION;
 ///////////////////////////////////////////////////////////
 // network protocol versioning                           //
 //                                                       //
-static const int PROTOCOL_VERSION =       180275;        //
+static const int PROTOCOL_VERSION =       180276;        //
 // disconnect from peers older than this proto version   //
-static const int MIN_PEER_PROTO_VERSION = 180275;        // 
+static const int MIN_PEER_PROTO_VERSION = 180276;        // 
 ///////////////////////////////////////////////////////////
 // intial proto version, to be increased after           //
 // version/verack negotiation                            //
@@ -43,7 +43,7 @@ static const int CADDR_TIME_VERSION = 180275;            //
 // only request blocks from nodes outside this range of  //
 // versions                                              //
 static const int NOBLKS_VERSION_START = 1;               //
-static const int NOBLKS_VERSION_END = 180274;            // 
+static const int NOBLKS_VERSION_END = 180275;            // 
 ///////////////////////////////////////////////////////////
 //
 // database format versioning
