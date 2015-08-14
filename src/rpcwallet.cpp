0d1053d5dba4bdd0a87890532266dfe8c4f1b3eb
Gridcoin <contact@gridcoin.us>
2015-08-14 18:45:06 -0500

    3.4.7.7/MinorVersion=294 - Leisure - Testnet
    Gridcoin Research 3.4.7.7/294/MSI=28.5
    Leisure Upgrade
    (Mandatory for TestNet)
    
    - Fixed mintlimiter rule in TestNet keeping nodes from staking
    - Added MiningErrors8 to getmininginfo and to overview.  If you are
    probing coin age for more than 10 minutes, this field will show what is
    going on in the stakeminer.
    If it says, Kernel Found 0: that means coin age is not mature enough.
    After approx 5 mins, Kernel Found should increase to a portion of the
    coins in the Stake bucket.  Once that happens the node will
    stake as soon as it passes the mintlimiter threshhold for the diff
    level.  Tested in testnet successfully (we should see more staking).
    - Added Lifetime Average Magnitude to list rsa.  This will be valuable
    to see the long term payments per day per magnitude in the future.

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -59,7 +59,8 @@ bool IsPoR2(double amt)
 	std::string sAmt = RoundToString(amt,8);
 	if (sAmt.length() > 8)
 	{
-		if (sAmt.substr(sAmt.length()-4,4)=="0124")
+		std::string suffix = sAmt.substr(sAmt.length()-4,4);
+		if (suffix == "0124" || suffix=="0123")
 		{
 			return true;
 		}
