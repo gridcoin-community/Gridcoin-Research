5197beae4aed1f486a65f6b8e707b788ef13ae5d
Marco Nilsson <denravonska@gmail.com>
2017-02-20 10:17:28 +0100

    Add missing include guard.

--- src/gridcoin.h
+++ src/gridcoin.h
@@ -1,3 +1,4 @@
+#pragma once
 
 static const std::string BoincHashMerkleRootNew = "ElimZa7b8c9ateXr9kgueTheJ2HackersExa192";
 static const std::string BoincHashWindowsMerkleRootNew = "yG3uv41o6n7apYOVVszTMQ==";
