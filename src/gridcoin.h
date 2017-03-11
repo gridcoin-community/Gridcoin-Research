87881640f1e90bab6443001ddaec1c03c5e48487
Marco Nilsson <denravonska@gmail.com>
2017-03-11 07:45:37 +0100

    Remove unreferenced GetBlocksCommand.

--- src/gridcoin.h
+++ src/gridcoin.h
@@ -2,8 +2,6 @@
 
 static const std::string BoincHashMerkleRootNew = "ElimZa7b8c9ateXr9kgueTheJ2HackersExa192";
 static const std::string BoincHashWindowsMerkleRootNew = "yG3uv41o6n7apYOVVszTMQ==";
-static const std::string GetBlocksCommand = "unknown";
-
 //Notes for manually compiled nodes:
 
 //addnode=node.gridcoin.us
