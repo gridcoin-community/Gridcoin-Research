b16f2b04f998de1fb3ceff31689f2b7806c1d6b2
skcin <huppdiwupp@mailbox.org>
2017-03-15 09:14:27 +0100

    Replace the obsolete QDesktopServices::storageLocation with QStandardPaths::writableLocation

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -72,7 +72,7 @@
 #include <QDateTime>
 #include <QMovie>
 #include <QFileDialog>
-#include <QDesktopServices>
+#include <QStandardPaths>
 #include <QTimer>
 #include <QDragEnterEvent>
 #include <QUrl>
@@ -2110,7 +2110,7 @@ void BitcoinGUI::encryptWallet(bool status)
 
 void BitcoinGUI::backupWallet()
 {
-    QString saveDir = QDesktopServices::storageLocation(QDesktopServices::DocumentsLocation);
+    QString saveDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
     QString filename = QFileDialog::getSaveFileName(this, tr("Backup Wallet"), saveDir, tr("Wallet Data (*.dat)"));
     if(!filename.isEmpty()) {
         if(!walletModel->backupWallet(filename)) {
