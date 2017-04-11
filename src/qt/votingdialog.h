5f43d3549ed2d32fe3dca4278a8d33a809c63814
Richard Leckinger <caraka3@gmail.com>
2017-04-11 17:21:00 +1200

    Update votingdialog.h
    QTCharts => 5.8, as per .pro file.

--- src/qt/votingdialog.h
+++ src/qt/votingdialog.h
@@ -26,7 +26,7 @@
 #  include <QtGui>
 #endif
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)
 #include <QtCharts/QChartGlobal>
 #endif
 
@@ -36,7 +36,7 @@ class QObject;
 class QResizeEvent;
 QT_END_NAMESPACE
 
-#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)
 QT_CHARTS_BEGIN_NAMESPACE
 class QChart;
 QT_CHARTS_END_NAMESPACE
