863ac753d7c19557864372a62616a96de54dd172
Foggyx420 <eastendmumbles@gmail.com>
2017-06-03 10:03:02 -0700

    Forgot transactiondesc.cpp

--- src/qt/transactiondesc.cpp
+++ src/qt/transactiondesc.cpp
@@ -276,7 +276,7 @@ QString TransactionDesc::toHTML(CWallet *wallet, CWalletTx &wtx)
 				       "<br>" + tr("Block Type") + ":</b> " + RoundToString(out_blocktype,0).c_str() + 
 					   "<br>" + tr("Block Number") + ":</b> " + RoundToString(out_blocknumber,0).c_str() +
 					   "<br>" + tr("RAC")           + ":</b> " + RoundToString(out_rac,0).c_str() +
-					   	"<br><br>" + tr("Gridcoin generated coins must mature 120 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.") + "<br>";
+					   	"<br><br>" + tr("Gridcoin generated coins must mature 110 blocks before they can be spent. When you generated this block, it was broadcast to the network to be added to the block chain. If it fails to get into the chain, its state will change to \"not accepted\" and it won't be spendable. This may occasionally happen if another node generates a block within a few seconds of yours.") + "<br>";
 
 
 	}
