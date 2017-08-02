d52cd004d6ae9c25050231afcf13e6011fecb6d5
Marco Nilsson <denravonska@gmail.com>
2017-08-02 14:29:42 +0200

    Fix Qt5 building in Windows.
    - Upgrade Qt to 5.8.0.
    - Use mingw from the Qt5 installation.
    - Add libpng.
    - Upgrade OpenSSL to 1.0.2l.
    - Upgrade boost to 1.64.0
    - Enable qrencode. This closes #199.

--- contrib/easywinbuilder/3a_build_boost.bat
+++ contrib/easywinbuilder/3a_build_boost.bat
@@ -3,20 +3,10 @@
 @cd %ROOTPATH%\%EWBLIBS%\%BOOST%
 
 @echo bootstrap...
-call bootstrap.bat mingw
+call bootstrap.bat gcc
 @echo.
 @echo.
 @echo building...
-b2.exe --build-type=minimal --with-chrono --with-filesystem --with-program_options --with-system --with-thread^
- --layout=versioned -sNO_BZIP2=1 -sNO_ZLIB=1^
- variant=release^
- toolset=gcc^
- link=static^
- threading=multi^
- target-os=windows^
- threadapi=win32^
- cxxflags="%ADDITIONALCCFLAGS%"^
- cflags="%ADDITIONALCCFLAGS%"^
- stage
+b2.exe --build-type=minimal --with-chrono --with-filesystem --with-program_options --with-system --with-thread toolset=gcc variant=release link=static threading=multi runtime-link=static --layout=versioned -sNO_BZIP2=1 -sNO_ZLIB=1 target-os=windows threadapi=win32 cxxflags="%ADDITIONALCCFLAGS%" cflags="%ADDITIONALCCFLAGS%" stage
 @cd ..\..\%EWBPATH%
 @if not "%RUNALL%"=="1" pause
--- contrib/easywinbuilder/set_vars.bat
+++ contrib/easywinbuilder/set_vars.bat
@@ -1,26 +1,24 @@
-@set PATH=a:\mingw\msys\1.0\bin;a:\mingw\bin
-
 @set LANG=en_US.UTF8
 @set LC_ALL=en_US.UTF8
 
 @set MINGWINSTALLER=mingw-get-inst-20120426
 
-@set OPENSSL=openssl-1.0.1e
+@set OPENSSL=openssl-1.0.2l
 @set BERKELEYDB=db-4.8.30.NC
-@set BOOST=boost_1_55_0
-@set BOOSTVERSION=1.55.0
+@set BOOST=boost_1_64_0
+@set BOOSTVERSION=1.64.0
 @set LEVELDB=leveldb
-@rem If you wonder why there is no -s- see: https://github.com/bitcoin/bitcoin/pull/2835#issuecomment-21231694
-@set BOOSTSUFFIX=-mgw46-mt-1_55
+@set BOOSTSUFFIX=-mgw53-mt-s-1_64
 @set MINIUPNPC=miniupnpc-1.9
+@set QRENCODE=qrencode-3.4.4
+@set LIBPNG=lpng1629
+@set ZLIB=zlib
 
 @set EWBLIBS=libs
 
-
-set QTDIR=A:\Qt\4.8.4
-set QMAKESPEC=A:\Qt\4.8.4\mkspecs\win32-g++
-set PATH=%PATH%;a:\qt\4.8.4\qmake\bin
-@set QTPATH=a:\Qt\4.8.4\bin
+@set QTDIR=A:\Qt\Qt5.8.0
+@set PATH=%QTDIR%\Tools\mingw530_32\bin;%QTDIR%\5.8\mingw53_32\bin;%PATH%
+@set QMAKESPEC=%QTDIR%\5.8\mingw53_32\mkspecs\win32-g++
 
 @set EWBPATH=contrib/easywinbuilder
 set ROOTPATH=..\..
@@ -37,8 +35,10 @@ xset ROOTPATH=a:\gridcoin-research
 @set QTDOWNLOADPATH=http://download.qt-project.org/official_releases/qt/4.8/4.8.5/qt-win-opensource-4.8.5-mingw.exe
 @rem Qt5 will need changes in gather_dlls.bat
 
-@set MSYS=a:/MinGW/msys/1.0/bin
-@set PERL=%MSYS%/perl.exe
+rem @set MINGW=%QTMINGW%
+@set MSYS=a:\msys\1.0\bin
+@set PERL=a:/msys/1.0/bin/perl.exe
+set PATH=%MSYS%;%PATH%
 
 @rem the following will be set as additional CXXFLAGS and CFLAGS for everything - no ' or ", space is ok
 @set ADDITIONALCCFLAGS=-fno-guess-branch-probability -frandom-seed=1984 -Wno-unused-variable -Wno-unused-value -Wno-sign-compare -Wno-strict-aliasing
