9935f80e023684ae0fc6c3c43e64ea9bd1da102d
TheCharlatan <seb.kung@gmail.com>
2017-08-13 19:19:23 +0200

    Remove unused dependencies

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,8 +1,7 @@
-packages:=boost openssl libevent zeromq curl libzip
+packages:=boost openssl curl libzip
 native_packages := native_ccache
 
-qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf zlib
+qt_packages = qrencode zlib
 
 qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
