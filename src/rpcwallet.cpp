005a0fdf423f3f890a2f4986f82ef803958cb050
TheCharlatan <seb.kung@gmail.com>
2017-11-26 20:37:55 +0100

    Move contents of clientversion.h to version.h
    To simplify the versioning system clientversion.h was removed. Files requiring access to versioning now include version.h

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "clientversion.h"
+#include "version.h"
 #include "txdb.h"
 #include "wallet.h"
 #include "walletdb.h"
