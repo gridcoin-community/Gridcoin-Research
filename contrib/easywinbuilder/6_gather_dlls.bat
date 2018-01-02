46fac6e0971c9d110fc51441c7476ae1b1bec532
Paul Jensen <eastendmumbles@gmail.com>
2018-01-02 10:56:35 -0800

    Add other dlls to copy for release. Fix libstc++-6.dll not being found as requires quotations

--- contrib/easywinbuilder/6_gather_dlls.bat
+++ contrib/easywinbuilder/6_gather_dlls.bat
@@ -1,11 +1,13 @@
 @call set_vars.bat
-copy %QTPATH%\Qt5Charts.dll %ROOTPATH%\release\
-copy %QTPATH%\Qt5Core.dll %ROOTPATH%\release\
-copy %QTPATH%\Qt5Gui.dll %ROOTPATH%\release\
-copy %QTPATH%\Qt5Network.dll %ROOTPATH%\release\
-copy %QTPATH%\Qt5Widgets.dll %ROOTPATH%\release\
-copy %QTPATH%\libgcc_s_dw2-1.dll %ROOTPATH%\release\
-copy %QTPATH%\libstdc++-6.dll %ROOTPATH%\release\
-copy %QTPATH%\libgcc_s_dw2-1.dll %ROOTPATH%\release\
-copy %QTPATH%\libwinpthread-1.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\Qt5Charts.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\Qt5Core.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\Qt5Gui.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\Qt5Network.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\Qt5Widgets.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\bin\libgcc_s_dw2-1.dll %ROOTPATH%\release\
+copy "%QTDIR%\5.8\mingw53_32\bin\libstdc++-6.dll" %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\bin\libwinpthread-1.dll %ROOTPATH%\release\
+copy %QTDIR%\5.8\mingw53_32\plugins\iconengines\qsvgicon.dll %ROOTPATH%\release\iconengines\qsvgicon.dll
+copy %QTDIR%\5.8\mingw53_32\plugins\imageformats\qsvg.dll %ROOTPATH%\release\imageformats\qsvg.dll
+copy %QTDIR%\5.8\mingw53_32\plugins\platforms\qwindows.dll %ROOTPATH%\release\platforms\qwindows.dll
 @if not "%RUNALL%"=="1" pause
