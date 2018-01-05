69d121eead4242c62d9a985351afbabc796e42ad
Marco Nilsson <denravonska@gmail.com>
2018-01-05 14:43:34 +0100

    Clear cache sections before testing.

--- src/test/appcache_tests.cpp
+++ src/test/appcache_tests.cpp
@@ -6,23 +6,24 @@ BOOST_AUTO_TEST_SUITE(appcache_tests)
 
 BOOST_AUTO_TEST_CASE(appcache_WrittenCacheShouldBeReadable)
 {
+    ClearCache("section");
     WriteCache("section", "key", "hello", 123456789);
     BOOST_CHECK(ReadCache("section", "key").value == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(appcache_ClearCacheShouldClearEntireSection)
 {
+    ClearCache("section");
     WriteCache("section", "key1", "hello", 123456789);
     WriteCache("section", "key2", "hello", 123456789);
     ClearCache("section");
     BOOST_CHECK(ReadCache("section", "key1").value.empty() == true);
     BOOST_CHECK(ReadCache("section", "key2").value.empty() == true);
-    DeleteCache("section","key1");
-    DeleteCache("section","key2");
 }
 
 BOOST_AUTO_TEST_CASE(appcache_KeyShouldBeEmptyAfterDeleteCache)
 {
+    ClearCache("section");
     WriteCache("section", "key", "hello", 123456789);
     DeleteCache("section", "key");
     BOOST_CHECK(ReadCache("section", "key").value.empty() == true);
@@ -31,21 +32,22 @@ BOOST_AUTO_TEST_CASE(appcache_KeyShouldBeEmptyAfterDeleteCache)
 
 BOOST_AUTO_TEST_CASE(appcache_GetCountOfShouldCountEntries)
 {
-   WriteCache("section", "key1", "hello", 123456789);
-   WriteCache("section", "key2", "hello", 123456789);
-   WriteCache("section", "key3", "hello", 123456789);
-   BOOST_CHECK_EQUAL(GetCountOf("section"), 3);
+    ClearCache("section");
+    WriteCache("section", "key1", "hello", 123456789);
+    WriteCache("section", "key2", "hello", 123456789);
+    WriteCache("section", "key3", "hello", 123456789);
+    BOOST_CHECK_EQUAL(GetCountOf("section"), 3);
 }
 
 BOOST_AUTO_TEST_CASE(appcache_GetListOfBeaconShouldIgnoreInvestors)
 {
+    ClearCache("beacon");
+    WriteCache("beacon", "CPID1", "INVESTOR", 12345);
+    BOOST_CHECK(GetListOf("beacon").empty() == true);
 
-   WriteCache("beacon", "CPID1", "INVESTOR", 12345);
-   BOOST_CHECK(GetListOf("beacon").empty() == true);
-
-   WriteCache("beacon", "CPID2", "abc123", 12345);
-   BOOST_CHECK(GetListOf("beacon").empty() == false);
-   BOOST_CHECK(GetListOf("beacon").find("abc123") != std::string::npos);
+    WriteCache("beacon", "CPID2", "abc123", 12345);
+    BOOST_CHECK(GetListOf("beacon").empty() == false);
+    BOOST_CHECK(GetListOf("beacon").find("abc123") != std::string::npos);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
