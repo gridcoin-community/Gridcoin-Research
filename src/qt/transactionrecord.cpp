f5e80813bdb6d9943e5f2913c9347ec8bc512785
Tomáš Brada <tomasbrod@azet.sk>
2018-04-27 13:04:14 +0200

    Replace OutputDebugStringF

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -201,7 +201,7 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
                                 if (vTxNormalInfo.second == "Vote" || vTxNormalInfo.second == "Add Poll")
                                 {
                                     if (fDebug)
-                                        OutputDebugStringF("Vote/Add Poll\n");
+                                        LogPrintf("Vote/Add Poll\n");
 
                                     sub.type = TransactionRecord::Vote;
                                     break;
