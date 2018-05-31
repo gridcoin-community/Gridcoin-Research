39bbcc278bc7e4591c97de0dc05c213b37d4cc50
jamescowens <jamesowens@optonline.net>
2018-05-30 21:11:14 -0400

    Implementation of slightly reduced nTimeOffset
    This multiplies the median of the sampled time offsets by 0.95 to take into account one's own node at 5% confidence versus rest of network at 95% confidence. It also assigns this directly to nTimeOffset (since it is not a true median anymore) and cleans up the following if statement.
    
    This sole change will provide a gentle restoring force to return and keep the overall offset average network-wide at zero compared to the underlying consensus of system clocks. Note that since the majority of nodes are synced to UTC via some form of synchronization, such as ntp on the node, this should act to keep the network roughly in sync with UTC.

--- src/util.cpp
+++ src/util.cpp
@@ -1317,17 +1317,15 @@ void AddTimeData(const CNetAddr& ip, int64_t nTime)
     if (fDebug10) LogPrintf("Added time data, samples %d, offset %+" PRId64 " (%+" PRId64 " minutes)\n", vTimeOffsets.size(), nOffsetSample, nOffsetSample/60);
     if (vTimeOffsets.size() >= 5 && vTimeOffsets.size() % 2 == 1)
     {
-        int64_t nMedian = vTimeOffsets.median();
+        // We believe the median of the other nodes 95% and our own node's time ("0" initial offset) 5%. This will also act to gently converge the network to consensus UTC, in case
+        // the entire network is displaced for some reason.
+        int64_t nTimeOffset = 0.95 * vTimeOffsets.median();
         std::vector<int64_t> vSorted = vTimeOffsets.sorted();
         // Only let other nodes change our time by so much
-        if (abs64(nMedian) < 70 * 60)
-        {
-            nTimeOffset = nMedian;
-        }
-        else
+        if (abs64(nTimeOffset) >= 70 * 60)
         {
             nTimeOffset = 0;
-
+        
             static bool fDone;
             if (!fDone)
             {
