0c024c2f6cc350b4ef144365c0c2d59117c19686
Sebastian Kung <seb.kung@gmail.com>
2018-06-04 23:48:46 +0200

    Fix rpc client
    Rpc client was not tested during the univalue conversion after multiple
    rebases. This fixes a typo and inverted boolean.

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -259,13 +259,13 @@ int CommandLineRPC(int argc, char *argv[])
         UniValue params = RPCConvertValues(strMethod, strParams);
 
         // Execute
-        UniValue reply = CallRPC(strMethod, params);
+        const UniValue reply = CallRPC(strMethod, params);
 
         // Parse reply
         const UniValue& result = find_value(reply, "result");
         const UniValue& error  = find_value(reply, "error");
 
-        if (error.isNull())
+        if (!error.isNull())
         {
             // Error
             strPrint = "error: " + error.write();
