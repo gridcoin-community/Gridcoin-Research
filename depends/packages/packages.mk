2ef02698c69b0497dbd4999b1b26e9c32fbbe5fc
TheCharlatan <seb.kung@gmail.com>
2018-07-17 23:54:44 +0200

    Add aarch64 qt depends support for cross compiling bitcoin-qt

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,15 +4,11 @@ native_packages := native_ccache
 qt_packages = qrencode
 
 ifeq ($(QT_59),1)
-qt_x86_64_linux_packages:=qt59 expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
-qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
-
+qt_linux_packages:=qt59 expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_darwin_packages=qt59
 qt_mingw32_packages=qt59
 else
-qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
-qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
-
+qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 endif
