gridcoin-community/Gridcoin-Research

commit 40d2a1c6d2f81f3484f2321b86f6178fd977c983

Remove genesis check in CheckBlock.

src/main.cpp
@@ -4164,10 +4164,6 @@ bool CBlock::AddToBlockIndex(unsigned int nFile, unsigned int nBlockPos, const u
 
 bool CBlock::CheckBlock(std::string sCaller, int height1, int64_t Mint, bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig, bool fLoadingIndex) const
 {
-
-    if (GetHash() == (fTestNet ? hashGenesisBlockTestNet : hashGenesisBlock))
-        return true;
-
     // These are checks that are independent of context
     // that can be verified before saving an orphan block.
 

commit 8f8673d728e675f61a57813b10d1bdc38c1639ef

Re-add genesis block check.
This fixes a regression introduced in 40d2a1c6d2f81f3484f2321b86f6178fd977c983
where the genesis block actually didn't pass the checks. This caused it to be
rejected and subsequent asserts to fail.

The new version is slightly more efficient as the cost is a uint256
comparison+hash check for the genesis block and just a uint256 comparison for
all other blocks.

src/main.cpp
@@ -4036,6 +4036,11 @@ bool CBlock::AddToBlockIndex(unsigned int nFile, unsigned int nBlockPos, const u
 
 bool CBlock::CheckBlock(std::string sCaller, int height1, int64_t Mint, bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig, bool fLoadingIndex) const
 {
+    // Allow the genesis block to pass.
+    if(hashPrevBlock == uint256() &&
+       GetHash() == (fTestNet ? hashGenesisBlockTestNet : hashGenesisBlock))
+        return true;
+
     // These are checks that are independent of context
     // that can be verified before saving an orphan block.
 