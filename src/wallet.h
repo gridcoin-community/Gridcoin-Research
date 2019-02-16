6ff052a404e79d3a4cdd60bb596c2804330ad5de
jamescowens <jamesowens@optonline.net>
2019-02-16 15:11:46 -0500

    Change GenerateType to GetGeneratedType for a better
    match to what the function is doing.

--- src/wallet.h
+++ src/wallet.h
@@ -948,5 +948,5 @@ private:
 
 bool GetWalletFile(CWallet* pwallet, std::string &strWalletFileOut);
 
-MinedType GenerateType(const uint256& tx, unsigned int vout);
+MinedType GetGeneratedType(const uint256& tx, unsigned int vout);
 #endif
