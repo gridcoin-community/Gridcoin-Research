91797b4834d55dd23893bae0044a6e39f0aae6cf
Marco Nilsson <denravonska@gmail.com>
2019-02-18 06:22:22 +0100

    Remove SystemCommand.

--- src/main.cpp
+++ src/main.cpp
@@ -8430,21 +8430,6 @@ std::string GetQuorumHash(const std::string& data)
     return RetrieveMd5(sHashIn);
 }
 
-std::string SystemCommand(const char* cmd)
-{
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) return "ERROR";
-    char buffer[128];
-    std::string result = "";
-    while(!feof(pipe))
-    {
-        if(fgets(buffer, 128, pipe) != NULL)
-            result += buffer;
-    }
-    pclose(pipe);
-    return result;
-}
-
 bool IsContract(CBlockIndex* pIndex)
 {
     return pIndex->nIsContract==1 ? true : false;
