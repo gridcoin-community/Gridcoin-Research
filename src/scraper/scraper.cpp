72b9a2eb596c139f2fc7fa2e49b536f1bf96ad56
jamescowens <jamesowens@optonline.net>
2019-03-14 15:58:12 -0400

    Changes to quiet logs and reduce output of listmanifests
    A number of log entries have been moved to fDebug, and others
    moved to fDebug3 to quiet the log entries.
    
    The rpc call listmanifests now accepts a boolean that defaults to
    false which controls whether an abbreviated manifest listing is
    produced, or a full JSON dump.

--- src/scraper/scraper.cpp
+++ src/scraper/scraper.cpp
@@ -454,7 +454,7 @@ public:
                 vuserpass.push_back(std::make_pair(vlist[0], vlist[1]));
             }
 
-            _log(logattribute::INFO, "userpass_data_import", "Userpass contains " + std::to_string(vuserpass.size()) + " projects");
+            if (fDebug3) _log(logattribute::INFO, "userpass_data_import", "Userpass contains " + std::to_string(vuserpass.size()) + " projects");
 
             return true;
         }
@@ -522,7 +522,7 @@ public:
 
             oauthdata.write(outdata.value().c_str(), outdata.size());
 
-            _log(logattribute::INFO, "auth_data_export", "Exported");
+            if (fDebug3) _log(logattribute::INFO, "auth_data_export", "Exported");
 
             return true;
         }
@@ -601,7 +601,7 @@ void ScraperApplyAppCacheEntries()
     ApplyCache("TEAM_WHITELIST", TEAM_WHITELIST);
     ApplyCache("SCRAPER_DEAUTHORIZED_BANSCORE_GRACE_PERIOD", SCRAPER_DEAUTHORIZED_BANSCORE_GRACE_PERIOD);
 
-    if (fDebug)
+    if (fDebug3)
     {
         _log(logattribute::INFO, "ScraperApplyAppCacheEntries", "scrapersleep = " + std::to_string(nScraperSleep));
         _log(logattribute::INFO, "ScraperApplyAppCacheEntries", "activebeforesb = " + std::to_string(nActiveBeforeSB));
@@ -658,15 +658,6 @@ void Scraper(bool bSingleShot)
         _log(logattribute::INFO, "Scraper", "Starting Scraper thread.");
     else
         _log(logattribute::INFO, "Scraper", "Running in single shot mode.");
-
-    // Hash check
-    std::string sHashCheck = "Hello world";
-    uint256 nHashCheck = Hash(sHashCheck.begin(), sHashCheck.end());
-
-    if (nHashCheck.GetHex() == "fe65ee46e01d18cebec555978abe82e021e5399171ce470e464996114d72dcf6")
-        _log(logattribute::INFO, "Scraper", "Hash for \"Hello world\" is " + Hash(sHashCheck.begin(), sHashCheck.end()).GetHex() + " and is correct.");
-    else
-        _log(logattribute::ERR, "Scraper", "Hash for \"Hello world\" is " + Hash(sHashCheck.begin(), sHashCheck.end()).GetHex() + " and is NOT correct.");
 
     uint256 nmScraperFileManifestHash = 0;
 
@@ -846,7 +837,7 @@ void Scraper(bool bSingleShot)
 
             DownloadProjectRacFilesByCPID();
 
-            _log(logattribute::INFO, "Scraper", "download size so far: " + std::to_string(ndownloadsize) + " upload size so far: " + std::to_string(nuploadsize));
+            if (fDebug) _log(logattribute::INFO, "Scraper", "download size so far: " + std::to_string(ndownloadsize) + " upload size so far: " + std::to_string(nuploadsize));
 
             ScraperStats mScraperStats = GetScraperStatsByConsensusBeaconList();
 
@@ -940,7 +931,7 @@ void NeuralNetwork()
 
 
     _log(logattribute::INFO, "NeuralNetwork", "Starting Neural Network housekeeping thread (new C++ implementation). \n"
-                                              "Note that this does NOT mean the NN is active. This simply does housekeeping"
+                                              "Note that this does NOT mean the NN is active. This simply does housekeeping "
                                               "functions.");
 
     while(!fShutdown)
@@ -2938,12 +2929,12 @@ bool ScraperSaveCScraperManifestToFiles(uint256 nManifestHash)
         if (fDebug3) _log(logattribute::INFO, "ENDLOCK", "cs_Scraper");
     }
 
-    fs::path savepath = pathScraper / "incoming";
+    fs::path savepath = pathScraper / "manifest_dump";
 
-    // Check to see if the Scraper incoming directory exists and is a directory. If not create it.
+    // Check to see if the Scraper manifest_dump directory exists and is a directory. If not create it.
     if (fs::exists(savepath))
     {
-        // If it is a normal file, this is not right. Remove the file and replace with the Scraper directory.
+        // If it is a normal file, this is not right. Remove the file and replace with the directory.
         if (fs::is_regular_file(savepath))
         {
             fs::remove(savepath);
@@ -2952,6 +2943,23 @@ bool ScraperSaveCScraperManifestToFiles(uint256 nManifestHash)
     }
     else
         fs::create_directory(savepath);
+
+    // Add on the hash subdirectory to the path. 7 digits of the hash is good enough.
+    savepath = savepath / nManifestHash.GetHex().substr(0, 7);
+
+    // Check to see if the Scraper manifest_dump/hash directory exists and is a directory. If not create it.
+    if (fs::exists(savepath))
+    {
+        // If it is a normal file, this is not right. Remove the file and replace with the directory.
+        if (fs::is_regular_file(savepath))
+        {
+            fs::remove(savepath);
+            fs::create_directory(savepath);
+        }
+    }
+    else
+        fs::create_directory(savepath);
+
 
     LOCK(CScraperManifest::cs_mapManifest);
     if (fDebug3) _log(logattribute::INFO, "LOCK", "CScraperManifest::cs_mapManifest");
