1ce6513ac130271c3b66d9049ddc903f0246563f
jamescowens <jamesowens@optonline.net>
2019-03-21 20:39:55 -0400

    Eliminate degenerate if statements for GetPast/FutureDrift

--- src/main.h
+++ src/main.h
@@ -132,8 +132,8 @@ inline bool IsV9Enabled_Tally(int nHeight)
     return IsV9Enabled(nHeight-120);
 }
 
-inline int64_t PastDrift(int64_t nTime, int nHeight)   { return IsProtocolV2(nHeight) ? nTime - 20 * 60  : nTime - 20 * 60; }
-inline int64_t FutureDrift(int64_t nTime, int nHeight) { return IsProtocolV2(nHeight) ? nTime + 20 * 60  : nTime + 20 * 60; }
+inline int64_t PastDrift(int64_t nTime, int nHeight)   { return nTime - 20 * 60; }
+inline int64_t FutureDrift(int64_t nTime, int nHeight) { return nTime + 20 * 60; }
 inline unsigned int GetTargetSpacing(int nHeight) { return IsProtocolV2(nHeight) ? 90 : 60; }
 
 extern bool IsNeuralNodeParticipant(const std::string& addr, int64_t locktime);
@@ -1455,10 +1455,7 @@ public:
 
     int64_t GetPastTimeLimit() const
     {
-        if (IsProtocolV2(nHeight))
-	         return GetMedianTimePast();
-        else
-            return GetMedianTimePast();
+        return GetMedianTimePast();
     }
 
     enum { nMedianTimeSpan=11 };
