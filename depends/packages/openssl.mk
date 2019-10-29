081b23e554c666cb34812878695d043ae60bf2d7
jamescowens <jamesowens@optonline.net>
2019-10-28 23:07:56 -0400

    Upgrade OpenSSL to 1.1.1d for depends and remove openssl
    functionality from Qt. We do not actually use Qt level opensssl
    functionality. All OpenSSL functionality in the wallet is in the core

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,69 +1,50 @@
 package=openssl
-GCCFLAGS?=
-$(package)_version=1.0.1k
+$(package)_version=1.1.1d
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=8f9faeaebad088e772f4ef5e38252d472be4d878c6b3a2718c10a4fcebe7a41c
+$(package)_sha256_hash=1e3a91bc1f9dfce01af26026f856e064eab4c8ee0a8f457b5ae30b40b8b711f2
 
 define $(package)_set_vars
-  $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
-  $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
-  $(package)_config_opts+=no-camellia
-  $(package)_config_opts+=no-capieng
-  $(package)_config_opts+=no-cast
-  $(package)_config_opts+=no-comp
-  $(package)_config_opts+=no-dso
-  $(package)_config_opts+=no-dtls1
-  $(package)_config_opts+=no-ec_nistp_64_gcc_128
-  $(package)_config_opts+=no-gost
-  $(package)_config_opts+=no-gmp
-  $(package)_config_opts+=no-heartbeats
-  $(package)_config_opts+=no-idea
-  $(package)_config_opts+=no-jpake
-  $(package)_config_opts+=no-krb5
-  $(package)_config_opts+=no-libunbound
-  $(package)_config_opts+=no-md2
-  $(package)_config_opts+=no-mdc2
-  $(package)_config_opts+=no-rc4
-  $(package)_config_opts+=no-rc5
-  $(package)_config_opts+=no-rdrand
-  $(package)_config_opts+=no-rfc3779
-  $(package)_config_opts+=no-rsax
-  $(package)_config_opts+=no-sctp
-  $(package)_config_opts+=no-seed
-  $(package)_config_opts+=no-sha0
-  $(package)_config_opts+=no-shared
-  $(package)_config_opts+=no-ssl-trace
-  $(package)_config_opts+=no-ssl2
-  $(package)_config_opts+=no-ssl3
-  $(package)_config_opts+=no-static_engine
-  $(package)_config_opts+=no-store
-  $(package)_config_opts+=no-unit-test
-  $(package)_config_opts+=no-weak-ssl-ciphers
-  $(package)_config_opts+=no-whirlpool
-  $(package)_config_opts+=no-zlib
-  $(package)_config_opts+=no-zlib-dynamic
-  $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
-  $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
-  $(package)_config_opts_x86_64_linux=linux-x86_64
-  $(package)_config_opts_i686_linux=linux-generic32
-  $(package)_config_opts_arm_linux=linux-generic32
-  $(package)_config_opts_aarch64_linux=linux-generic64
-  $(package)_config_opts_mipsel_linux=linux-generic32
-  $(package)_config_opts_mips_linux=linux-generic32
-  $(package)_config_opts_powerpc_linux=linux-generic32
-  $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
-  $(package)_config_opts_x86_64_mingw32=mingw64
-  $(package)_config_opts_i686_mingw32=mingw
-  $(package)_cxxflags_aarch64_linux = $(GCCFLAGS)
-  $(package)_cflags_aarch64_linux = $(GCCFLAGS)
-  $(package)_cxxflags_arm_linux = $(GCCFLAGS)
-  $(package)_cflags_arm_linux = $(GCCFLAGS)
-endef
-
-define $(package)_preprocess_cmds
-  sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
-  sed -i.old "s|engines apps test|engines|" Makefile.org
+$(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
+$(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
+$(package)_config_opts+=no-camellia
+$(package)_config_opts+=no-capieng
+$(package)_config_opts+=no-cast
+$(package)_config_opts+=no-comp
+$(package)_config_opts+=no-dso
+$(package)_config_opts+=no-dtls1
+$(package)_config_opts+=no-ec_nistp_64_gcc_128
+$(package)_config_opts+=no-gost
+$(package)_config_opts+=no-heartbeats
+$(package)_config_opts+=no-idea
+$(package)_config_opts+=no-md2
+$(package)_config_opts+=no-mdc2
+$(package)_config_opts+=no-rc4
+$(package)_config_opts+=no-rc5
+$(package)_config_opts+=no-rdrand
+$(package)_config_opts+=no-rfc3779
+$(package)_config_opts+=no-sctp
+$(package)_config_opts+=no-seed
+$(package)_config_opts+=no-shared
+$(package)_config_opts+=no-ssl-trace
+$(package)_config_opts+=no-ssl3
+$(package)_config_opts+=no-unit-test
+$(package)_config_opts+=no-weak-ssl-ciphers
+$(package)_config_opts+=no-whirlpool
+$(package)_config_opts+=no-zlib
+$(package)_config_opts+=no-zlib-dynamic
+$(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
+$(package)_config_opts_linux=-fPIC -Wa,--noexecstack
+$(package)_config_opts_x86_64_linux=linux-x86_64
+$(package)_config_opts_i686_linux=linux-generic32
+$(package)_config_opts_arm_linux=linux-generic32
+$(package)_config_opts_aarch64_linux=linux-generic64
+$(package)_config_opts_mipsel_linux=linux-generic32
+$(package)_config_opts_mips_linux=linux-generic32
+$(package)_config_opts_powerpc_linux=linux-generic32
+$(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
+$(package)_config_opts_x86_64_mingw32=mingw64
+$(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_config_cmds
@@ -71,13 +52,9 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
+  $(MAKE) -j1 build_libs
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) -j1 install_sw
-endef
-
-define $(package)_postprocess_cmds
-  rm -rf share bin etc
+  $(MAKE) DESTDIR=$($(package)_staging_dir) -j1 install_dev
 endef
