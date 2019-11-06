44ab4b92a0652530db3e0a6b06ef72d470bf5c9e
Paul Jensen <eastendmumbles@gmail.com>
2019-11-06 14:44:36 -0800

    Hide options dialog checkbox when running testnet

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -5,6 +5,7 @@
 #include "bitcoinunits.h"
 #include "monitoreddatamapper.h"
 #include "optionsmodel.h"
+#include "init.h"
 
 #include <QDir>
 #include <QIntValidator>
@@ -88,6 +89,9 @@ OptionsDialog::OptionsDialog(QWidget *parent) :
     connect(mapper, SIGNAL(currentIndexChanged(int)), this, SLOT(disableApplyButton()));
     /* setup/change UI elements when proxy IP is invalid/valid */
     connect(this, SIGNAL(proxyIpValid(QValidatedLineEdit *, bool)), this, SLOT(handleProxyIpValid(QValidatedLineEdit *, bool)));
+
+    if (fTestNet)
+        ui->disableUpdateCheck->setHidden(true);
 }
 
 OptionsDialog::~OptionsDialog()
