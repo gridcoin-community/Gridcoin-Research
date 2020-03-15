4214e1b687cbf20834973f62b028c93db82e7e08
James C. Owens <jamesowens@optonline.net>
2020-03-14 21:19:07 -0400

    Implement new transaction fees for block version 11.

--- src/main.h
+++ src/main.h
@@ -744,6 +744,8 @@ public:
      */
     int64_t GetValueIn(const MapPrevTx& mapInputs) const;
 
+    int64_t GetBaseFee(enum GetMinFee_mode mode=GMF_BLOCK) const;
+
     int64_t GetMinFee(unsigned int nBlockSize=1, enum GetMinFee_mode mode=GMF_BLOCK, unsigned int nBytes = 0) const;
 
     bool ReadFromDisk(CDiskTxPos pos, FILE** pfileRet=NULL)
