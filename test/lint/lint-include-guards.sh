7181af4941c5f4ffada498c3a5651aac68196fac
Cy Rossignol <cy@rossignols.me>
2020-08-14 21:10:21 -0500

    Allow use of "#pragma once" as an include guard for lint
    Most Gridcoin-specific headers use "#pragma once" as an include guard.
    This changes the lint rules to allow "#pragma once" in addition to the
    standard "#ifndef"/"#define" macro guards.

--- test/lint/lint-include-guards.sh
+++ test/lint/lint-include-guards.sh
@@ -10,11 +10,16 @@ export LC_ALL=C
 HEADER_ID_PREFIX="BITCOIN_"
 HEADER_ID_SUFFIX="_H"
 
-REGEXP_EXCLUDE_FILES_WITH_PREFIX="src/(crypto/ctaes/|leveldb/|crc32c/|secp256k1/|test/fuzz/FuzzedDataProvider.h|tinyformat.h|bench/nanobench.h|univalue/)"
+REGEXP_EXCLUDE_FILES_WITH_PREFIX="src/(crypto/ctaes/|leveldb/|crc32c/|secp256k1/|test/(data|fuzz/FuzzedDataProvider.h)|tinyformat.h|bench/nanobench.h|univalue/)"
 
 EXIT_CODE=0
 for HEADER_FILE in $(git ls-files -- "*.h" | grep -vE "^${REGEXP_EXCLUDE_FILES_WITH_PREFIX}")
 do
+    # Gridcoin: allow use of the #pragma once directive as an include guard:
+    if grep -cE "^#pragma once" "${HEADER_FILE}" > /dev/null; then
+        continue
+    fi
+
     HEADER_ID_BASE=$(cut -f2- -d/ <<< "${HEADER_FILE}" | sed "s/\.h$//g" | tr / _ | tr "[:lower:]" "[:upper:]")
     HEADER_ID="${HEADER_ID_PREFIX}${HEADER_ID_BASE}${HEADER_ID_SUFFIX}"
     if [[ $(grep -cE "^#(ifndef|define) ${HEADER_ID}" "${HEADER_FILE}") != 2 ]]; then
