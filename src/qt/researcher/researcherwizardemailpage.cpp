e44aac7b752c76a8c8c39b0cef1562abbe4d9a7b
a123b <6329481+a123b@users.noreply.github.com>
2020-08-19 18:07:52 +0200

    Validate email address in researcher wizard

--- src/qt/researcher/researcherwizardemailpage.cpp
+++ src/qt/researcher/researcherwizardemailpage.cpp
@@ -2,6 +2,8 @@
 #include "qt/researcher/researchermodel.h"
 #include "qt/researcher/researcherwizardemailpage.h"
 
+#include <QRegularExpressionValidator>
+
 // -----------------------------------------------------------------------------
 // Class: ResearcherWizardEmailPage
 // -----------------------------------------------------------------------------
@@ -13,12 +15,18 @@ ResearcherWizardEmailPage::ResearcherWizardEmailPage(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // Validate email with the same pattern as the BOINC server
+    QRegularExpression boincEmailRegexp("^([^@]+)@([^@\\.]+)\\.([^@]{2,})$");
+    boincEmailValidator = new QRegularExpressionValidator(boincEmailRegexp, this);
+    ui->emailAddressLineEdit->setValidator(boincEmailValidator);
+
     // The asterisk denotes a mandatory field:
     registerField("emailAddress*", ui->emailAddressLineEdit);
 }
 
 ResearcherWizardEmailPage::~ResearcherWizardEmailPage()
 {
+    delete boincEmailValidator;
     delete ui;
 }
 
