f54286444c733b683158aaa4dbf29a7c87c78aca
Cy Rossignol <cy@rossignols.me>
2020-09-06 19:54:48 -0500

    Fix tests for _GLIBCXX_ASSERTIONS
    This fixes some tests to avoid triggering assertions when enabling
    _GLIBCXX_ASSERTIONS or _GLIBCXX_DEBUG.

--- src/script.cpp
+++ src/script.cpp
@@ -966,20 +966,20 @@ bool EvalScript(vector<vector<unsigned char> >& stack, const CScript& script, co
                     valtype& vch = stacktop(-1);
                     valtype vchHash((opcode == OP_RIPEMD160 || opcode == OP_SHA1 || opcode == OP_HASH160) ? 20 : 32);
                     if (opcode == OP_RIPEMD160)
-                        CRIPEMD160().Write(&vch[0], vch.size()).Finalize(&vchHash[0]);
+                        CRIPEMD160().Write(vch.data(), vch.size()).Finalize(vchHash.data());
                     else if (opcode == OP_SHA1)
-                        CSHA1().Write(&vch[0], vch.size()).Finalize(&vchHash[0]);
+                        CSHA1().Write(vch.data(), vch.size()).Finalize(vchHash.data());
                     else if (opcode == OP_SHA256)
-                        CSHA256().Write(&vch[0], vch.size()).Finalize(&vchHash[0]);
+                        CSHA256().Write(vch.data(), vch.size()).Finalize(vchHash.data());
                     else if (opcode == OP_HASH160)
                     {
                         uint160 hash160 = Hash160(vch);
-                        memcpy(&vchHash[0], &hash160, sizeof(hash160));
+                        memcpy(vchHash.data(), &hash160, sizeof(hash160));
                     }
                     else if (opcode == OP_HASH256)
                     {
                         uint256 hash = Hash(vch.begin(), vch.end());
-                        memcpy(&vchHash[0], &hash, sizeof(hash));
+                        memcpy(vchHash.data(), &hash, sizeof(hash));
                     }
                     popstack(stack);
                     stack.push_back(vchHash);
