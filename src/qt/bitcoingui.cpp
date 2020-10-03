912d115049df9b73ec1ad2c8cd1a10670c6a44cb
opsinphark <53201159+opsinphark@users.noreply.github.com>
2020-10-03 09:10:24 +0200

    gui: Update default font to Inter-Regular   - This font is licensed under the Open Font License.   - https://fonts.google.com/specimen/Inter?query=inter

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -110,6 +110,7 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
 
     setGeometry(QStyle::alignedRect(Qt::LeftToRight,Qt::AlignCenter,QDesktopWidget().availableGeometry(this).size() * 0.6,QDesktopWidget().availableGeometry(this)));
 
+    QFontDatabase::addApplicationFont(":/fonts/Inter-Regular.ttf");
     setWindowTitle(tr("Gridcoin") + " " + tr("Wallet"));
 
 #ifndef Q_OS_MAC
