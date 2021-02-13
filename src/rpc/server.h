f6f87b920c1a3b90ee703f0734d414843fc84650
div72 <60045611+div72@users.noreply.github.com>
2021-02-13 18:35:41 +0300

    rpc: add dumpbeaconcontracts

--- src/rpc/server.h
+++ src/rpc/server.h
@@ -191,6 +191,7 @@ extern UniValue currentcontractaverage(const UniValue& params, bool fHelp);
 extern UniValue debug(const UniValue& params, bool fHelp);
 extern UniValue debug10(const UniValue& params, bool fHelp);
 extern UniValue debug2(const UniValue& params, bool fHelp);
+extern UniValue dumpcontracts(const UniValue& params, bool fHelp);
 extern UniValue rpc_getblockstats(const UniValue& params, bool fHelp);
 extern UniValue getlistof(const UniValue& params, bool fHelp);
 extern UniValue inspectaccrualsnapshot(const UniValue& params, bool fHelp);
