2bfba5b7a009339460c68674ef00b8a0ba5a8c11
Cy Rossignol <cy@rossignols.me>
2021-03-06 16:40:26 -0600

    Re-enable CI for macOS GUI cross-compile
    Because we couldn't cross-compile the macOS GUI with the depends system,
    the CI configuration built the headless binary only. This re-enables the
    GUI portion of the build.

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -6,10 +6,9 @@
 
 export LC_ALL=C.UTF-8
 
-export HOST=x86_64-apple-darwin16
-export DOCKER_NAME_TAG=ubuntu:18.04  # Check that bionic can cross-compile to macos (bionic is used in the gitian build as well)
+export HOST=x86_64-apple-darwin18
 export GOAL="install"
-export GRIDCOIN_CONFIG="--with-gui=qt5 --enable-reduce-exports"
+export GRIDCOIN_CONFIG="--with-gui --enable-reduce-exports"
 export NEED_XVFB="true"
 export NO_DEPENDS=1
 export OSX_SDK=""
