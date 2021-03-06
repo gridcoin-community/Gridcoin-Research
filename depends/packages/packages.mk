ed5e1c109bee0bef68a4a1ca26fb32bdcbfd0ad7
Cy Rossignol <cy@rossignols.me>
2021-03-06 16:40:13 -0600

    Remove now unused macOS native depends recipes
    This removes:
      - native_biplist
      - native_cdrkit (replaced by native_libdmg-hfsplus)
    
    Bitcoin's newer macOS packaging tools do not need these dependencies
    anymore.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -15,8 +15,8 @@ endif
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_biplist native_ds_store native_mac_alias
+darwin_native_packages = native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_libdmg-hfsplus
 endif
