d1d338588f18b97956ef42e253c9a7e47989e581
barton26 <barton2526@gmail.com>
2021-03-18 03:35:43 -0400

    Update Copyright headers

--- src/gridcoin/staking/kernel.cpp
+++ src/gridcoin/staking/kernel.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2012-2013 The PPCoin developers
-// Copyright (c) 2014-2020 The Gridcoin developers
+// Copyright (c) 2014-2021 The Gridcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
