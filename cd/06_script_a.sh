689020e8a8e64b65f7645b093b908bf3069f6069
div72 <60045611+div72@users.noreply.github.com>
2021-08-04 21:30:12 +0300

    wip

--- cd/06_script_a.sh
+++ cd/06_script_a.sh
@@ -9,29 +9,30 @@ export LC_ALL=C.UTF-8
 GRIDCOIN_CONFIG_ALL="--disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
 DOCKER_EXEC "ccache --zero-stats --max-size=$CCACHE_SIZE"
 
+DOCKER_EXEC mkdir -p "${BASE_BUILD_DIR}"
+export P_CI_DIR="${BASE_BUILD_DIR}/gridcoin-$HOST"
+
 BEGIN_FOLD autogen
 if [ -n "$CONFIG_SHELL" ]; then
-  DOCKER_EXEC "$CONFIG_SHELL" -c "./autogen.sh"
+  DOCKER_EXEC "$CONFIG_SHELL" -c "{BASE_ROOT_DIR}/autogen.sh"
 else
-  DOCKER_EXEC ./autogen.sh
+  DOCKER_EXEC ${BASE_ROOT_DIR}/autogen.sh
 fi
 END_FOLD
 
-DOCKER_EXEC mkdir -p "${BASE_BUILD_DIR}"
-export P_CI_DIR="${BASE_BUILD_DIR}"
+#export P_CI_DIR="${BASE_BUILD_DIR}"
+
+#BEGIN_FOLD configure
+#DOCKER_EXEC "${BASE_ROOT_DIR}/configure" --cache-file=config.cache $GRIDCOIN_CONFIG_ALL $GRIDCOIN_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
+#END_FOLD
+
+#BEGIN_FOLD distdir
+#DOCKER_EXEC make distdir VERSION=$HOST
+#END_FOLD
+
 
 BEGIN_FOLD configure
-DOCKER_EXEC "${BASE_ROOT_DIR}/configure" --cache-file=config.cache $GRIDCOIN_CONFIG_ALL $GRIDCOIN_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
-END_FOLD
-
-BEGIN_FOLD distdir
-DOCKER_EXEC make distdir VERSION=$HOST
-END_FOLD
-
-export P_CI_DIR="${BASE_BUILD_DIR}/gridcoin-$HOST"
-
-BEGIN_FOLD configure
-DOCKER_EXEC ./configure --cache-file=../config.cache $GRIDCOIN_CONFIG_ALL $GRIDCOIN_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
+DOCKER_EXEC ../configure $GRIDCOIN_CONFIG_ALL $GRIDCOIN_CONFIG || ( (DOCKER_EXEC cat config.log) && false)
 END_FOLD
 
 BEGIN_FOLD build
