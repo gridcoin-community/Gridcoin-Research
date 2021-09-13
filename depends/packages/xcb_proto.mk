10a6315cbd5390f3b735ef78f8d675a445c590ce
barton26 <barton2526@gmail.com>
2021-09-13 19:03:36 -0400

    depends: don't configure xcb_proto

--- depends/packages/xcb_proto.mk
+++ depends/packages/xcb_proto.mk
@@ -6,8 +6,6 @@ $(package)_file_name=xcb-proto-$($(package)_version).tar.bz2
 $(package)_sha256_hash=7ef40ddd855b750bc597d2a435da21e55e502a0fefa85b274f2c922800baaf05
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
-  $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags_aarch64_linux = $(GCCFLAGS)
   $(package)_cflags_aarch64_linux = $(GCCFLAGS)
   $(package)_cxxflags_arm_linux = $(GCCFLAGS)
