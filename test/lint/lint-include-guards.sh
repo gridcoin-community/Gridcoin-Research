f593cfc327d7a4f6aa30e517e79969976147bff5
div72 <60045611+div72@users.noreply.github.com>
2021-09-17 20:21:31 +0300

    lint: allow gridcoin specific include guards

--- test/lint/lint-include-guards.sh
+++ test/lint/lint-include-guards.sh
@@ -7,7 +7,7 @@
 # Check include guards.
 
 export LC_ALL=C
-HEADER_ID_PREFIX="BITCOIN_"
+HEADER_ID_PREFIX="(BIT|GRID)COIN_"
 HEADER_ID_SUFFIX="_H"
 
 REGEXP_EXCLUDE_FILES_WITH_PREFIX="src/(crypto/ctaes/|leveldb/|crc32c/|secp256k1/|test/(data|fuzz/FuzzedDataProvider.h)|tinyformat.h|bench/nanobench.h|univalue/)"
@@ -20,8 +20,15 @@ do
         continue
     fi
 
-    HEADER_ID_BASE=$(cut -f2- -d/ <<< "${HEADER_FILE}" | sed "s/\.h$//g" | tr / _ | tr "[:lower:]" "[:upper:]")
-    HEADER_ID="${HEADER_ID_PREFIX}${HEADER_ID_BASE}${HEADER_ID_SUFFIX}"
+    if [[ $HEADER_FILE == src/gridcoin/* ]]; then
+        # Gridcoin: allow src/gridcoin to be accessed with just GRIDCOIN_ prefix.
+        HEADER_ID_BASE=$(cut -f3- -d/ <<< "${HEADER_FILE}" | sed "s/\.h$//g" | tr / _ | tr "[:lower:]" "[:upper:]")
+        HEADER_ID="GRIDCOIN_${HEADER_ID_BASE}${HEADER_ID_SUFFIX}"
+    else
+        HEADER_ID_BASE=$(cut -f2- -d/ <<< "${HEADER_FILE}" | sed "s/\.h$//g" | tr / _ | tr "[:lower:]" "[:upper:]")
+        HEADER_ID="${HEADER_ID_PREFIX}${HEADER_ID_BASE}${HEADER_ID_SUFFIX}"
+    fi
+
     if [[ $(grep -cE "^#(ifndef|define) ${HEADER_ID}" "${HEADER_FILE}") != 2 ]]; then
         echo "${HEADER_FILE} seems to be missing the expected include guard:"
         echo "  #ifndef ${HEADER_ID}"
