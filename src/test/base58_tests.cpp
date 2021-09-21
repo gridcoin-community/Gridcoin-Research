daab3fc420382002b7910e657501b27a75860704
barton26 <barton2526@gmail.com>
2021-09-21 00:31:48 -0400

    Add Base58 tests.

--- src/test/base58_tests.cpp
+++ src/test/base58_tests.cpp
@@ -1,10 +1,15 @@
+// Copyright (c) 2011-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include <boost/test/unit_test.hpp>
 
-#include "base58.h"
-#include "util.h"
-#include "test/data/base58_encode_decode.json.h"
-#include "test/data/base58_keys_valid.json.h"
-#include "test/data/base58_keys_invalid.json.h"
+#include <base58.h>
+#include <util.h>
+#include <util/strencodings.h>
+#include <test/data/base58_encode_decode.json.h>
+#include <test/data/base58_keys_valid.json.h>
+#include <test/data/base58_keys_invalid.json.h>
 
 #include <univalue.h>
 
@@ -55,12 +60,24 @@ BOOST_AUTO_TEST_CASE(base58_DecodeBase58)
     }
 
     BOOST_CHECK(!DecodeBase58("invalid", result));
+    BOOST_CHECK(!DecodeBase58(std::string("invalid"), result));
+    BOOST_CHECK(!DecodeBase58(std::string("\0invalid", 8), result));
+
+    BOOST_CHECK(DecodeBase58(std::string("good", 4), result));
+    BOOST_CHECK(!DecodeBase58(std::string("bad0IOl", 7), result));
+    BOOST_CHECK(!DecodeBase58(std::string("goodbad0IOl", 11), result));
+    BOOST_CHECK(!DecodeBase58(std::string("good\0bad0IOl", 12), result));
 
     // check that DecodeBase58 skips whitespace, but still fails with unexpected non-whitespace at the end.
     BOOST_CHECK(!DecodeBase58(" \t\n\v\f\r skip \r\f\v\n\t a", result));
     BOOST_CHECK( DecodeBase58(" \t\n\v\f\r skip \r\f\v\n\t ", result));
     std::vector<unsigned char> expected = ParseHex("971a55");
     BOOST_CHECK_EQUAL_COLLECTIONS(result.begin(), result.end(), expected.begin(), expected.end());
+
+    BOOST_CHECK(DecodeBase58Check(std::string("3vQB7B6MrGQZaxCuFg4oh", 21), result));
+    BOOST_CHECK(!DecodeBase58Check(std::string("3vQB7B6MrGQZaxCuFg4oi", 21), result));
+    BOOST_CHECK(!DecodeBase58Check(std::string("3vQB7B6MrGQZaxCuFg4oh0IOl", 25), result));
+    BOOST_CHECK(!DecodeBase58Check(std::string("3vQB7B6MrGQZaxCuFg4oh\00IOl", 26), result));
 }
 
 // Visitor to check address type
