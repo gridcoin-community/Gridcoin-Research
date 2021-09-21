35edc8c07d3ae857e502b105162c32b3e442ebf2
barton26 <barton2526@gmail.com>
2021-09-20 23:17:57 -0400

    Add tests from upstream. Check negative

--- src/test/base32_tests.cpp
+++ src/test/base32_tests.cpp
@@ -1,6 +1,13 @@
-#include <boost/test/unit_test.hpp>
+// Copyright (c) 2012-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "util.h"
+#include <util/strencodings.h>
+
+#include <boost/test/unit_test.hpp>
+#include <string>
+
+using namespace std::literals;
 
 BOOST_AUTO_TEST_SUITE(base32_tests)
 
@@ -8,13 +15,24 @@ BOOST_AUTO_TEST_CASE(base32_testvectors)
 {
     static const std::string vstrIn[]  = {"","f","fo","foo","foob","fooba","foobar"};
     static const std::string vstrOut[] = {"","my======","mzxq====","mzxw6===","mzxw6yq=","mzxw6ytb","mzxw6ytboi======"};
-    for (unsigned int i = 0; i < std::size(vstrIn); i++)
+    for (unsigned int i=0; i<std::size(vstrIn); i++)
     {
         std::string strEnc = EncodeBase32(vstrIn[i]);
         BOOST_CHECK(strEnc == vstrOut[i]);
         std::string strDec = DecodeBase32(vstrOut[i]);
         BOOST_CHECK(strDec == vstrIn[i]);
     }
+
+    // Decoding strings with embedded NUL characters should fail
+    bool failure;
+    (void)DecodeBase32("invalid\0"s, &failure); // correct size, invalid due to \0
+    BOOST_CHECK(failure);
+    (void)DecodeBase32("AWSX3VPP"s, &failure); // valid
+    BOOST_CHECK(!failure);
+    (void)DecodeBase32("AWSX3VPP\0invalid"s, &failure); // correct size, invalid due to \0
+    BOOST_CHECK(failure);
+    (void)DecodeBase32("AWSX3VPPinvalid"s, &failure); // invalid size
+    BOOST_CHECK(failure);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
