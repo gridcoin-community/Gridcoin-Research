235a6b07bdd32b30e0d01d446fd7a05b33ce34af
barton26 <barton2526@gmail.com>
2021-09-27 17:15:43 -0400

    build: remove glibc back compat

--- ci/test/00_setup_env_arm.sh
+++ ci/test/00_setup_env_arm.sh
@@ -25,6 +25,6 @@ export RUN_FUNCTIONAL_TESTS=false
 export GOAL="install"
 # -Wno-psabi is to disable ABI warnings: "note: parameter passing for argument of type ... changed in GCC 7.1"
 # This could be removed once the ABI change warning does not show up by default
-export GRIDCOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CXXFLAGS=-Wno-psabi --enable-werror"
+export GRIDCOIN_CONFIG="--enable-reduce-exports CXXFLAGS=-Wno-psabi --enable-werror"
 # Disable QT as it takes too long to compile on CI
 export DEP_OPTS="NO_QT=1"
