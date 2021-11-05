gridcoin-community/Gridcoin-Research

commit a5de7c91c641151c74ed523edd86aeaf197ccebb

refactor: separate clientversion from version

src/clientversion.cpp
@@ -1,0 +1,75 @@
+// Copyright (c) 2012-2020 The Bitcoin Core developers
+// Copyright (c) 2021 The Gridcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <clientversion.h>
+
+#include <tinyformat.h>
+
+
+/**
+ * Name of client reported in the 'version' message. Report the same name
+ * for both gridcoinresearchd and gridcoinresearch-qt, to make it harder for attackers to
+ * target servers or GUI users specifically.
+ */
+const std::string CLIENT_NAME("Halford");
+
+
+#ifdef HAVE_BUILD_INFO
+#include "build.h"
+// The <obj/build.h>, which is generated by the build environment (share/genbuild.sh),
+// could contain only one line of the following:
+//   - "#define BUILD_GIT_TAG ...", if the top commit is tagged
+//   - "#define BUILD_GIT_COMMIT ...", if the top commit is not tagged
+//   - "// No build information available", if proper git information is not available
+#endif
+
+//! git will put "#define GIT_COMMIT_ID ..." on the next line inside archives. $Format:%n#define GIT_COMMIT_ID "%H"$
+
+#ifdef BUILD_GIT_TAG
+    #define BUILD_DESC BUILD_GIT_TAG
+    #define BUILD_SUFFIX ""
+#else
+    #define BUILD_DESC "v" PACKAGE_VERSION
+    #if CLIENT_VERSION_IS_RELEASE
+        #define BUILD_SUFFIX ""
+    #elif defined(BUILD_GIT_COMMIT)
+        #define BUILD_SUFFIX "-" BUILD_GIT_COMMIT
+    #elif defined(GIT_COMMIT_ID)
+        #define BUILD_SUFFIX "-g" GIT_COMMIT_ID
+    #else
+        #define BUILD_SUFFIX "-unk"
+    #endif
+#endif
+
+static std::string FormatVersion(int nVersion)
+{
+    return strprintf("%d.%d.%d", nVersion / 10000, (nVersion / 100) % 100, nVersion % 100);
+}
+
+std::string FormatFullVersion()
+{
+    static const std::string CLIENT_BUILD(BUILD_DESC BUILD_SUFFIX);
+    return CLIENT_BUILD;
+}
+
+/**
+ * Format the subversion field according to BIP 14 spec (https://github.com/bitcoin/bips/blob/master/bip-0014.mediawiki)
+ */
+std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments)
+{
+    std::ostringstream ss;
+    ss << "/";
+    ss << name << ":" << FormatVersion(nClientVersion);
+    if (!comments.empty())
+    {
+        std::vector<std::string>::const_iterator it(comments.begin());
+        ss << "(" << *it;
+        for(++it; it != comments.end(); ++it)
+            ss << "; " << *it;
+        ss << ")";
+    }
+    ss << "/";
+    return ss.str();
+}

.gitattributes
@@ -1,4 +1,4 @@
-src/version.cpp export-subst
+src/clientversion.cpp export-subst
 
 # Auto detect text files and perform LF normalization
 *        text=auto

Makefile.am
@@ -61,7 +61,7 @@ nobase_icons_DATA = $(DIST_ICONS)
 endif
 
 dist-hook:
-	-$(GIT) archive --format=tar HEAD -- src/version.cpp | $(AMTAR) -C $(top_distdir) -xf -
+	-$(GIT) archive --format=tar HEAD -- src/clientversion.cpp | $(AMTAR) -C $(top_distdir) -xf -
 
 $(BITCOIN_WIN_INSTALLER): all-recursive
 	$(MKDIR_P) $(top_builddir)/release

src/Makefile.am
@@ -73,6 +73,7 @@ GRIDCOIN_CORE_H = \
     chainparams.h \
     chainparamsbase.h \
     checkpoints.h \
+    clientversion.h \
     compat.h \
     compat/assumptions.h \
     compat/byteswap.h \
@@ -203,6 +204,7 @@ GRIDCOIN_CORE_CPP = addrdb.cpp \
     chainparams.cpp \
     chainparamsbase.cpp \
     checkpoints.cpp \
+    clientversion.cpp \
     consensus/merkle.cpp \
     consensus/tx_verify.cpp \
     crypter.cpp \
@@ -282,7 +284,6 @@ GRIDCOIN_CORE_CPP = addrdb.cpp \
     util/time.cpp \
     util.cpp \
     validation.cpp \
-    version.cpp \
     wallet/db.cpp \
     wallet/rpcdump.cpp \
     wallet/rpcwallet.cpp \
@@ -294,7 +295,7 @@ obj/build.h: FORCE
 	@$(MKDIR_P) $(builddir)/obj
 	@$(top_srcdir)/share/genbuild.sh "$(abs_top_builddir)/src/obj/build.h" \
 	  "$(abs_top_srcdir)"
-libgridcoin_util_a-version.$(OBJEXT): obj/build.h
+libgridcoin_util_a-clientversion.$(OBJEXT): obj/build.h
 
 # util: shared between all executables.
 # This library *must* be included to make sure that the glibc

src/addrdb.cpp
@@ -7,7 +7,7 @@
 
 #include <addrman.h>
 #include <chainparams.h>
-// #include <clientversion.h>
+#include <clientversion.h>
 #include <hash.h>
 // #include <random.h>
 // #include <streams.h>

src/alert.cpp
@@ -10,6 +10,7 @@
 
 #include "alert.h"
 #include "chainparams.h"
+#include "clientversion.h"
 #include "key.h"
 #include "net.h"
 #include "streams.h"

src/clientversion.h
@@ -1,0 +1,47 @@
+// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Copyright (c) 2021 The Gridcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CLIENTVERSION_H
+#define BITCOIN_CLIENTVERSION_H
+
+#include <util/macros.h>
+
+#if defined(HAVE_CONFIG_H)
+#include <config/gridcoin-config.h>
+#endif //HAVE_CONFIG_H
+
+// Check that required client information is defined
+#if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
+#error Client version information missing: version is not defined by gridcoin-config.h or in any other way
+#endif
+
+//! Copyright string used in Windows .rc files
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
+
+/**
+ * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
+ * WINDRES_PREPROC is defined to indicate that its pre-processor is running.
+ * Anything other than a define should be guarded below.
+ */
+
+#if !defined(WINDRES_PREPROC)
+
+#include <string>
+#include <vector>
+
+static const int CLIENT_VERSION =
+                             10000 * CLIENT_VERSION_MAJOR
+                         +     100 * CLIENT_VERSION_MINOR
+                         +       1 * CLIENT_VERSION_BUILD;
+
+extern const std::string CLIENT_NAME;
+
+
+std::string FormatFullVersion();
+std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
+
+#endif // WINDRES_PREPROC
+
+#endif // BITCOIN_CLIENTVERSION_H

src/dbwrapper.cpp
@@ -5,14 +5,13 @@
 
 #include <map>
 
-#include <boost/version.hpp>
-
 #include <leveldb/env.h>
 #include <leveldb/cache.h>
 #include <leveldb/filter_policy.h>
 #include <leveldb/helpers/memenv/memenv.h>
 
 #include "chainparams.h"
+#include "clientversion.h"
 #include "gridcoin/staking/kernel.h"
 #include "txdb.h"
 #include "main.h"

commit d297b285889e949ab763184a56f6d42dcd24728f

util: fix FormatVersion
`FormatVersion` function was broken in
a5de7c91c641151c74ed523edd86aeaf197ccebb which results in user agent
strings to be formatted weirdly and may result in alerts falsely being
ignored.

src/clientversion.cpp
@@ -46,7 +46,10 @@ const std::string CLIENT_NAME("Halford");
 
 static std::string FormatVersion(int nVersion)
 {
-    return strprintf("%d.%d.%d", nVersion / 10000, (nVersion / 100) % 100, nVersion % 100);
+    if (nVersion % 100 == 0) {
+        return strprintf("%d.%d.%d", nVersion / 1000000, (nVersion / 10000) % 100, (nVersion / 100) % 100);
+    }
+    return strprintf("%d.%d.%d.%d", nVersion / 1000000, (nVersion / 10000) % 100, (nVersion / 100) % 100, nVersion % 100);
 }
 
 std::string FormatFullVersion()