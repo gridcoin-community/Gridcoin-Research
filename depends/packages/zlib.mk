d8ad3d97a2284abf7ae15fdf163109253e1c780c
div72 <60045611+div72@users.noreply.github.com>
2021-11-27 21:31:16 +0300

    wip

--- depends/packages/zlib.mk
+++ depends/packages/zlib.mk
@@ -18,7 +18,7 @@ endef
 # CFLAGS, RANLIB, AR, and ARFLAGS from the environment rather than from
 # command-line arguments.
 define $(package)_config_cmds
-  env $($(package)_config_opts) V=1 ./configure --static --prefix=$(host_prefix) || (find . | grep config.log)
+  env $($(package)_config_opts) V=1 ./configure --static --prefix=$(host_prefix) || (cat configure.log; false)
 endef
 
 define $(package)_build_cmds
