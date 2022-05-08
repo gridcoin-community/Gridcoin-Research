85a0a3f978739d9f1e618654ee65d9bb4b051f4e
jamescowens <jamesowens@optonline.net>
2022-05-08 13:42:05 -0400

    Add mrcmodel.h/cpp skeletons

---
+++ src/qt/mrcmodel.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2014-2022 The Gridcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php.
+
+#ifndef GRIDCOIN_QT_MRCMODEL_H
+#define GRIDCOIN_QT_MRCMODEL_H
+
+
+enum class MRCRequestStatus
+{
+    PENDING,
+    REJECTED_QUEUE_FULL,
+    STALE,
+    DELETED
+};
+
+
+#endif // GRIDCOIN_QT_MRCMODEL_H
