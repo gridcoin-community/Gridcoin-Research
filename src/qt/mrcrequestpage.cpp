abe3ca58de0f029d380fd826c0fbdd179c3312e6
jamescowens <jamesowens@optonline.net>
2022-05-08 13:51:18 -0400

    Add mrcrequestpage.h/cpp skeletons

---
+++ src/qt/mrcrequestpage.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2014-2022 The Gridcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php.
+
+#include "qt/mrcrequestpage.h"
---
+++ src/qt/mrcrequestpage.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2014-2022 The Gridcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php.
+
+#ifndef GRIDCOIN_QT_MRCREQUESTPAGE_H
+#define GRIDCOIN_QT_MRCREQUESTPAGE_H
+
+
+
+#endif // GRIDCOIN_QT_MRCREQUESTPAGE_H
