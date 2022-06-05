c73b23961f758371875a71695a104b4011f892a3
Paul Jensen <eastendmumbles@gmail.com>
2022-06-05 11:57:15 -0700

    Requested init.cpp change

--- src/init.cpp
+++ src/init.cpp
@@ -432,7 +432,7 @@ void SetupServerArgs()
     // Wallet
     argsman.AddArg("-upgradewallet", "Upgrade wallet to latest format",
                    ArgsManager::ALLOW_ANY, OptionsCategory::WALLET);
-    argsman.AddArg("-keypool=<n>", "Set key pool size to <n> (default: 1000)",
+    argsman.AddArg("-keypool=<n>", strprintf("Set key pool size to <n> (default: %u)", DEFAULT_KEYPOOL_SIZE),
                    ArgsManager::ALLOW_ANY, OptionsCategory::WALLET);
     argsman.AddArg("-rescan", "Rescan the block chain for missing wallet transactions",
                    ArgsManager::ALLOW_ANY, OptionsCategory::WALLET);
