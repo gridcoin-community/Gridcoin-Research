d63dd87f8d986e876777bde4d59e84a3edb0f1d5
div72 <60045611+div72@users.noreply.github.com>
2022-12-30 20:13:23 +0300

    docs, ci: remove mentions to bdb flags

--- ci/test/00_setup_env_mac_host.sh
+++ ci/test/00_setup_env_mac_host.sh
@@ -8,14 +8,7 @@ export LC_ALL=C.UTF-8
 
 export HOST=x86_64-apple-darwin
 export GOAL="install"
-# We run the contrib/install_db4.sh script rather than installing the
-# Homebrew berkeley-db4 formula to add the Berkeley DB 4.8 dependency
-# to avoid a quirk with macOS on GitHub Actions. This script compiles
-# BDB with the "--disable-replication" flag. The tests failed because
-# BDB complained that "replication requires locking support". We need
-# to point the compiler at the local BDB installation:
-export BDB_PREFIX="${BASE_ROOT_DIR}/db4"
-export GRIDCOIN_CONFIG="--with-gui --enable-reduce-exports BDB_LIBS='-L${BDB_PREFIX}/lib -ldb_cxx-4.8' BDB_CFLAGS='-I${BDB_PREFIX}/include'"
+export GRIDCOIN_CONFIG="--with-gui --enable-reduce-exports"
 export NEED_XVFB="true"
 export NO_DEPENDS=1
 export OSX_SDK=""
