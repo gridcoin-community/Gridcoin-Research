2c249c43abd95135280b452508a978fcc2507ac5
div72 <60045611+div72@users.noreply.github.com>
2023-03-22 01:09:13 +0300

    contrib: add nix file
    Rationale:
        Creates a development environment that can compile Gridcoin when
        used with nix-shell command. Could be useful for Nix users.

---
+++ contrib/default.nix
@@ -0,0 +1,13 @@
+let pkgs = import <nixpkgs> {};
+in
+with pkgs.stdenv;
+with pkgs.stdenv.lib;
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [ autoreconfHook libtool pkg-config qt5.wrapQtAppsHook ];
+  buildInputs = with pkgs; [ boost openssl libevent curl qt5.qttools libzip qrencode ];
+
+  shellHook = ''
+    echo "Run configure with: --with-qt-bindir=${pkgs.qt5.qtbase.dev}/bin:${pkgs.qt5.qttools.dev}/bin --with-boost-libdir=${pkgs.boost.out}/lib"
+  '';
+}
