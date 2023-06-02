ff5defdc9526be5fa8e9a7bb105ef992e720ad75
Anna “CyberTailor” <cyber@sysrq.in>
2023-06-03 02:36:42 +0500

    build: CMake: Fix strerror_r check

--- build-aux/cmake/CheckStrerrorR.cmake
+++ build-aux/cmake/CheckStrerrorR.cmake
@@ -1,9 +1,14 @@
+include(CheckCCompilerFlag)
 include(CheckCSourceRuns)
 include(CheckSymbolExists)
 
 check_symbol_exists(strerror_r "string.h" HAVE_STRERROR_R)
 
 if(HAVE_STRERROR_R)
+    check_c_compiler_flag(-Werror HAS_WERROR)
+    if(HAS_WERROR)
+        set(CMAKE_REQUIRED_FLAGS "-Werror")
+    endif()
     check_c_source_runs("
         #include <errno.h>
         #include <string.h>
@@ -17,4 +22,5 @@ if(HAVE_STRERROR_R)
         }"
         STRERROR_R_CHAR_P
     )
+    set(CMAKE_REQUIRED_FLAGS "")
 endif()
