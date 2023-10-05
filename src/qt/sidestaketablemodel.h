4da4867b325901daa257e4eacfe122d2bdc89f33
James C. Owens <jamesowens@optonline.net>
2023-10-05 13:43:01 -0400

    Create skeletons for sidestaketablemodel.h/cpp

---
+++ src/qt/sidestaketablemodel.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2014-2023 The Gridcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or https://opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_SIDESTAKETABLEMODEL_H
+#define BITCOIN_QT_SIDESTAKETABLEMODEL_H
+
+#endif // BITCOIN_QT_SIDESTAKETABLEMODEL_H
