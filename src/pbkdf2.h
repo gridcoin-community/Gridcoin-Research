979ca56b4cd1c02df7d65025e000a482970aa1d4
div72 <noreply@div72.xyz>
2023-10-08 20:03:06 +0300

    crypto: use vendored HMAC-SHA256 implementation for PBKDF2

--- src/pbkdf2.h
+++ src/pbkdf2.h
@@ -3,22 +3,9 @@
 #ifndef BITCOIN_PBKDF2_H
 #define BITCOIN_PBKDF2_H
 
-#include <openssl/sha.h>
+#include <crypto/hmac_sha256.h>
+
 #include <stdint.h>
-
-typedef struct HMAC_SHA256Context {
-    SHA256_CTX ictx;
-    SHA256_CTX octx;
-} HMAC_SHA256_CTX;
-
-void
-HMAC_SHA256_Init(HMAC_SHA256_CTX * ctx, const void * _K, size_t Klen);
-
-void
-HMAC_SHA256_Update(HMAC_SHA256_CTX * ctx, const void *in, size_t len);
-
-void
-HMAC_SHA256_Final(unsigned char digest[32], HMAC_SHA256_CTX * ctx);
 
 void
 PBKDF2_SHA256(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt,
