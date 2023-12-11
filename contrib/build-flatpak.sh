3805d21e0b8c14f994227c9e4ce609cc696d4d5f
İlker Avcı <ikolomiko@gmail.com>
2023-12-12 00:32:45 +0300

    contrib: Add script to build a Flatpak bundle

---
+++ contrib/build-flatpak.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+SECONDS=0
+
+repo_root="$(realpath $(dirname "$0")/..)"
+mkdir -p "$repo_root/build"
+cd "$repo_root/build"
+
+echo $repo_root
+flatpak-builder flatpak-build "$repo_root/contrib/flatpak-manifest.json" --force-clean
+flatpak build-export gridcoin-flatpak flatpak-build
+flatpak build-bundle gridcoin-flatpak Gridcoin-Research.flatpak us.gridcoin.Gridcoin-Research
+
+echo "Took $SECONDS seconds to build the Flatpak bundle."
