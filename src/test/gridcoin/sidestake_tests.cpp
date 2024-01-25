4028b24bfd00a30f5069331884d12e67429d836a
James C. Owens <jamesowens@optonline.net>
2024-01-24 23:17:13 -0500

    Tweaks to Fraction and Sidestake unit tests

--- src/test/gridcoin/sidestake_tests.cpp
+++ src/test/gridcoin/sidestake_tests.cpp
@@ -22,6 +22,30 @@ BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_trivial)
     BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 0);
 }
 
+BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double_below_minimum)
+{
+    GRC::Allocation allocation((double) 0.0000499999);
+
+    BOOST_CHECK_EQUAL(allocation.IsSimplified(), true);
+    BOOST_CHECK_EQUAL(allocation.IsZero(), true);
+    BOOST_CHECK_EQUAL(allocation.IsPositive(), false);
+    BOOST_CHECK_EQUAL(allocation.IsNonNegative(), true);
+    BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 0);
+}
+
+BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double_minimum)
+{
+    GRC::Allocation allocation((double) 0.0001);
+
+    BOOST_CHECK_EQUAL(allocation.GetNumerator(), 1);
+    BOOST_CHECK_EQUAL(allocation.GetDenominator(), 10000);
+    BOOST_CHECK_EQUAL(allocation.IsSimplified(), true);
+    BOOST_CHECK_EQUAL(allocation.IsZero(), false);
+    BOOST_CHECK_EQUAL(allocation.IsPositive(), true);
+    BOOST_CHECK_EQUAL(allocation.IsNonNegative(), true);
+    BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 0);
+}
+
 BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double)
 {
     GRC::Allocation allocation((double) 0.0005);
@@ -33,6 +57,45 @@ BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double)
     BOOST_CHECK_EQUAL(allocation.IsPositive(), true);
     BOOST_CHECK_EQUAL(allocation.IsNonNegative(), true);
     BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 0);
+}
+
+BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double_one_percent)
+{
+    GRC::Allocation allocation((double) 0.01);
+
+    BOOST_CHECK_EQUAL(allocation.GetNumerator(), 1);
+    BOOST_CHECK_EQUAL(allocation.GetDenominator(), 100);
+    BOOST_CHECK_EQUAL(allocation.IsSimplified(), true);
+    BOOST_CHECK_EQUAL(allocation.IsZero(), false);
+    BOOST_CHECK_EQUAL(allocation.IsPositive(), true);
+    BOOST_CHECK_EQUAL(allocation.IsNonNegative(), true);
+    BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 0);
+}
+
+BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double_just_below_unity)
+{
+    GRC::Allocation allocation((double) 0.9999);
+
+    BOOST_CHECK_EQUAL(allocation.GetNumerator(), 9999);
+    BOOST_CHECK_EQUAL(allocation.GetDenominator(), 10000);
+    BOOST_CHECK_EQUAL(allocation.IsSimplified(), true);
+    BOOST_CHECK_EQUAL(allocation.IsZero(), false);
+    BOOST_CHECK_EQUAL(allocation.IsPositive(), true);
+    BOOST_CHECK_EQUAL(allocation.IsNonNegative(), true);
+    BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 0);
+}
+
+BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_double_maximum_before_multiplication)
+{
+    GRC::Allocation allocation((double) 1.0);
+
+    BOOST_CHECK_EQUAL(allocation.GetNumerator(), 1);
+    BOOST_CHECK_EQUAL(allocation.GetDenominator(), 1);
+    BOOST_CHECK_EQUAL(allocation.IsSimplified(), true);
+    BOOST_CHECK_EQUAL(allocation.IsZero(), false);
+    BOOST_CHECK_EQUAL(allocation.IsPositive(), true);
+    BOOST_CHECK_EQUAL(allocation.IsNonNegative(), true);
+    BOOST_CHECK_EQUAL(allocation.ToCAmount(), (CAmount) 1);
 }
 
 BOOST_AUTO_TEST_CASE(sidestake_Allocation_Initialization_from_fraction)
@@ -69,7 +132,7 @@ BOOST_AUTO_TEST_CASE(sidestake_Allocation_multiplication_and_derivation_of_alloc
 
     // Allocations that are initialized from doubles are rounded to the nearest 1/10000. This is the worst case
     // therefore, in terms of numerator and denominator.
-    GRC::Allocation allocation(Fraction(9999, 10000, true));
+    GRC::Allocation allocation(0.9999);
 
     BOOST_CHECK_EQUAL(allocation.GetNumerator(), 9999);
     BOOST_CHECK_EQUAL(allocation.GetDenominator(), 10000);
@@ -77,16 +140,8 @@ BOOST_AUTO_TEST_CASE(sidestake_Allocation_multiplication_and_derivation_of_alloc
 
     CAmount max_accrual = 16384 * COIN;
 
-    CAmount output = static_cast<GRC::Allocation>(allocation * max_accrual).ToCAmount();
-
-    BOOST_CHECK_EQUAL(output, int64_t {1638236160000});
-
-    GRC::Allocation computed_output_alloc(Fraction(output, max_accrual, true));
-
-    // This is what the mandatory sidestake validation does at its core. The actual check is >=
-    // because it is allowed for the output to have an allocation to the destination greater than required.
-    // This test is for exactness, so it is checking whether it is equal.
-    BOOST_CHECK(computed_output_alloc == allocation);
+    CAmount actual_output = static_cast<GRC::Allocation>(allocation * max_accrual).ToCAmount();
+    BOOST_CHECK_EQUAL(actual_output, int64_t {1638236160000});
 }
 
 BOOST_AUTO_TEST_SUITE_END()
