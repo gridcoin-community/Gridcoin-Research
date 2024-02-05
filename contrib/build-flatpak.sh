3c24a751b64d51ac36cb944e52927d27989bf2a5
İlker Avcı <31849478+ikolomiko@users.noreply.github.com>
2024-02-05 20:28:07 +0300

    Update contrib/build-flatpak.sh
    Surround the repo_root variable with double quotes
    
    Co-authored-by: div72 <60045611+div72@users.noreply.github.com>

--- contrib/build-flatpak.sh
+++ contrib/build-flatpak.sh
@@ -7,7 +7,7 @@ repo_root="$(realpath $(dirname "$0")/..)"
 mkdir -p "$repo_root/build"
 cd "$repo_root/build"
 
-echo $repo_root
+echo "$repo_root"
 flatpak-builder flatpak-build "$repo_root/contrib/flatpak-manifest.json" --force-clean
 flatpak build-export gridcoin-flatpak flatpak-build
 flatpak build-bundle gridcoin-flatpak Gridcoin-Research.flatpak us.gridcoin.Gridcoin-Research
