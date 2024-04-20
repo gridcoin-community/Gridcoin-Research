521bab7743abb8ee1356cf4190d29bda13c71767
div72 <noreply@div72.xyz>
2024-04-20 18:00:07 +0300

    cd: add support for macos aarch64

---
+++ cd/00_setup_env_mac_arm64_host.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019-2020 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export HOST=aarch64-apple-darwin
+export GRIDCOIN_CONFIG="--with-gui --enable-reduce-exports"
+export OSX_SDK=""
+export CCACHE_SIZE=300M
+export RUN_UNIT_TESTS=true
+export RUN_FUNCTIONAL_TESTS=false
