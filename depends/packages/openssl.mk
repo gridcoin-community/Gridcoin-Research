d7a8ac72a61cf53c8472bcb947860e2168f75668
div72 <noreply@div72.xyz>
2024-09-15 21:49:07 +0300

    depends: fix openssl compilation for arm64-darwin

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -5,8 +5,8 @@ $(package)_file_name=OpenSSL_$(subst .,_,$($(package)_version)).tar.gz
 $(package)_sha256_hash=dac036669576e83e8523afdb3971582f8b5d33993a2d6a5af87daa035f529b4f
 
 define $(package)_set_vars
-# OpenSSL relies on the asm keyword but that fails when it's not compiled with GNU extensions.
-$(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc) -Dasm=__asm__"
+# OpenSSL's Configure seem to ignore LDFLAGS so we pass it through the CC variable.
+$(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc) $($(package)_ldflags)"
 $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
 $(package)_config_opts+=no-camellia
 $(package)_config_opts+=no-capieng
@@ -34,7 +34,6 @@ $(package)_config_opts+=no-weak-ssl-ciphers
 $(package)_config_opts+=no-whirlpool
 $(package)_config_opts+=no-zlib
 $(package)_config_opts+=no-zlib-dynamic
-$(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
 $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
 $(package)_config_opts_x86_64_linux=linux-x86_64
 $(package)_config_opts_i686_linux=linux-generic32
