c7ad517f4e20f5b59568832024c953ee5bbeb472
div72 <noreply@div72.xyz>
2024-09-16 14:15:43 +0300

    cd: add clang for darwin cross job
    See 7b6007f6250f379edf1345ecc54a17e198c7c815.

--- cd/00_setup_env_mac.sh
+++ cd/00_setup_env_mac.sh
@@ -7,9 +7,9 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_macos_cross
-export DOCKER_NAME_TAG=ubuntu:20.04
+export DOCKER_NAME_TAG=ubuntu:24.04
 export HOST=x86_64-apple-darwin
-export PACKAGES="cmake imagemagick libcap-dev librsvg2-bin libz-dev libbz2-dev libtiff-tools libtinfo5 python3-dev python3-setuptools xorriso"
+export PACKAGES="clang lld llvm cmake imagemagick libcap-dev librsvg2-bin libz-dev libbz2-dev libtiff-tools python3-dev python3-setuptools xorriso"
 export XCODE_VERSION=15.0
 export XCODE_BUILD_ID=15A240d
 export GRIDCOIN_CONFIG="--with-gui --enable-reduce-exports"
