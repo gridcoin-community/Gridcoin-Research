31367a9f8e115e54aa638b96084c1d6d133bef81
James C. Owens <jamesowens@optonline.net>
2024-10-22 17:25:24 -0400

    Rewrite GRC::GetConstantBlockReward
    This refactors GetConstantBlockReward to use the blockchain
    consensus paramters, removes the lookback limitation for block
    v13+ to correspond with the removal of the contract
    lookback window for protocol entries, and corrects the behavior
    of the lookup if the input index is not at pIndexBest.

--- src/main.h
+++ src/main.h
@@ -47,8 +47,6 @@ class MRC;
 typedef std::optional<Claim> ClaimOption;
 }
 
-static const int64_t DEFAULT_CBR = 10 * COIN;
-
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
