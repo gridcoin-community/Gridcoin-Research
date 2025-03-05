gridcoin-community/Gridcoin-Research

commit 4d91c8c299b388c0891d7626d6c669d539e37cc4

[refactor] rpc: boost 1.87 fixes for server

src/rpc/server.cpp
@@ -500,15 +500,19 @@ bool ClientAllowed(const boost::asio::ip::address& address)
 {
     // Make sure that IPv4-compatible and IPv4-mapped IPv6 addresses are treated as IPv4 addresses
     if (address.is_v6()
-     && (address.to_v6().is_v4_compatible()
-      || address.to_v6().is_v4_mapped()))
-        return ClientAllowed(address.to_v6().to_v4());
+     && (address.to_v6() <= boost::asio::ip::make_address_v6("::ffff:ffff")
+      || address.to_v6().is_v4_mapped())) {
+        auto address6 = address.to_v6();
+        auto bytes = address6.to_bytes();
+
+        return ClientAllowed(boost::asio::ip::address_v4({bytes[12], bytes[13], bytes[14], bytes[15]}));
+    }
 
     if (address == asio::ip::address_v4::loopback()
      || address == asio::ip::address_v6::loopback()
      || (address.is_v4()
          // Check whether IPv4 addresses match 127.0.0.0/8 (loopback subnet)
-      && (address.to_v4().to_ulong() & 0xff000000) == 0x7f000000))
+      && (address.to_v4().to_bytes()[0] == 127)))
         return true;
 
     const string strAddress = address.to_string();
@@ -661,7 +665,7 @@ void StartRPCThreads()
         acceptor->set_option(boost::asio::ip::v6_only(loopback), v6_only_error);
 
         acceptor->bind(endpoint);
-        acceptor->listen(socket_base::max_connections);
+        acceptor->listen(socket_base::max_listen_connections);
 
         RPCListen(acceptor, *rpc_ssl_context, fUseSSL);
 
@@ -684,7 +688,7 @@ void StartRPCThreads()
             acceptor->open(endpoint.protocol());
             acceptor->set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
             acceptor->bind(endpoint);
-            acceptor->listen(socket_base::max_connections);
+            acceptor->listen(socket_base::max_listen_connections);
 
             RPCListen(acceptor, *rpc_ssl_context, fUseSSL);
 

commit a91d529389e88bc7ebe0795e7a2925026d498b1e

rpc: fix allow check for ::1
The ClientAllowed check broke in
4d91c8c299b388c0891d7626d6c669d539e37cc4 as the is_v4_compatible
function was mistakenly refactored to include ::1 and ::. Since ::1
(a.k.a the IPv6 localhost) got considered to be an IPv4 compatible IPv6
address, it got treated as IPv4 0.0.0.1 which caused the wallet to
reject connections from localhost on some systems.

src/rpc/server.cpp
@@ -501,7 +501,8 @@ bool ClientAllowed(const boost::asio::ip::address& address)
     // Make sure that IPv4-compatible and IPv4-mapped IPv6 addresses are treated as IPv4 addresses
     if (address.is_v6()
      && (address.to_v6() <= boost::asio::ip::make_address_v6("::ffff:ffff")
-      || address.to_v6().is_v4_mapped())) {
+      || address.to_v6().is_v4_mapped())
+     && !(address.to_v6() == asio::ip::address_v6::loopback() || address.to_v6() == asio::ip::make_address_v6("::"))) {
         auto address6 = address.to_v6();
         auto bytes = address6.to_bytes();
 